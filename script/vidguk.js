c910eb8b271ee7069469b468184ef33c5a3f66b0
Kirill Brovchenko <brovchenkokirill50@gmail.com>
2025-02-24 18:41:21 +0200

    fdgf

--- script/vidguk.js
+++ script/vidguk.js
@@ -51,38 +51,51 @@ const notes = [
 ];
 
 function render() {
-    // for (let i = 0; i < notes.length; i++) {
-    //     listElement.insertAdjacentHTML("beforeend", getNotesTemplate(notes[i]));
-    // }
-
-    for (let note of notes) {
-        listElement.insertAdjacentHTML('beforeend', getNotesTemplate(note))
+    listElement.innerHTML = ''
+    if (notes.length === 0) {
+        listElement.innerHTML = '<p>No elemnts<p>'
+    }
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML("beforeend", getNotesTemplate(notes[i], i));
     }
 }
 render()
 
 createBtn.onclick = function () {
-    if (inputElement.value.length === 0 || inputElement.value.length.trim) {
+    if (inputElement.value.length === 0) {
         return;
     }
     const newNote = {
         title: inputElement.value,
         complited: false,
     }
-    listElement.insertAdjacentHTML(
-        "beforeend",
-        getNotesTemplate(newNote)
-    );
-    inputElement.value = "";
+    notes.push(newNote)
+    render()
+    inputElement.value = '';
 }
 
-function getNotesTemplate(note) {
+listElement.onclick = function(event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+
+        if (type === 'toggle') {
+            notes[index].complited = !notes[index].complited
+        } else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+        
+        render()
+    }
+}
+
+function getNotesTemplate(note, index) {
     return `
     <li class="list-group-item d flex justify-content-between aling-items-center">
-        <span>${note.title}</span>
+        <span class='${note.complited ? 'text-decoration-line-through' : ''}'>${note.title}</span>
         <span class="redaktor">
-             <span class="btn btn-small btn-success">&check;</span>
-              <span class="btn btn-small btn-danger">&times;</span>
+             <span class="btn btn-small btn-${note.complited ? 'warning' : 'success'}" data-index='${index}' data-type='toggle'>&check;</span>
+              <span class="btn btn-small btn-danger" data-index='${index}' data-type='remove'>&times;</span>
         </span>
     </li>`;
 }
